ff81083b4db99a726d50739d217d3f53941db687
NakamaShp <khlfaalhsn5@gmail.com>
2025-11-08 20:59:17 +0700

    fixing supabase client

--- app/(main)/Pendaftaran/page.tsx
+++
@@ -1,9 +0,0 @@
-import PendaftaranForm from "@/components/pages/PendaftaranForm";
-
-export default function FormPendaftaran() {
-  return (
-    <main className="min-h-screen p-8">
-      <PendaftaranForm />
-    </main>
-  );
-}
